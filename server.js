3b396220381ef5ee8b1acca7f0c4670111ec732f
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-06-16 17:23:07 +0800

    Update the logic

--- server.js
+++ server.js
@@ -88,6 +88,33 @@ const output = events.reduce((model, event, index) => {
 // Output is an object dictionary that contains the date of the current events
 // console.log(output)
 
-Object.keys(output).map((date) => {
-  console.log(output[date].split('+'))
+const datesWithEvents = Object.keys(output).reduce((obj, date) => {
+  const events = output[date].split('+').filter(x => x.trim())
+  const eventsWithDates = events.map((event) => {
+    console.log('do something', event.split(/\b((\d.+\-))/img))
+    const [ date, url ] = event.split(' - ')
+    const hasData = /\[(.+?\r?\n?)\]\((.+?\r?\n?)\)$/img.exec(url)
+
+    if (!hasData) {
+    //   / console.log('no data =>', hasData, url, event)
+      return {}
+    }
+    const [ _, title, link ] = hasData
+    return {
+      date,
+      title,
+      link
+    }
+  })
+  obj[date] = eventsWithDates
+  return obj
+}, {})
+
+const eventsByMonths = Object.keys(datesWithEvents).map((key) => {
+  return {
+    date: key,
+    events: datesWithEvents[key]
+  }
 })
+
+console.log(eventsByMonths)
